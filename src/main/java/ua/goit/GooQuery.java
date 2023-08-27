4709b9820102086fb50cda45374747c54d5e2f27
Anton Vasylevskyi <vasilevskiy@gmail.com>
2023-08-27 14:04:59 +0300

    Added task 1

---
+++ src/main/java/ua/goit/GooQuery.java
@@ -0,0 +1,39 @@
+package ua.goit;
+
+public class GooQuery {
+
+    private final String language;
+    private final String text;
+
+    public GooQuery(String language, String text) {
+        this.language = language;
+        this.text = text;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getText() {
+        return text;
+    }
+    @Override
+    public final String toString() {
+        return "Searching [" + text + "], using language: " + language;
+    }
+}
+
+class GooQueryTest {
+    public static void main(String[] args) {
+        GooQuery query = new GooQuery("en", "capital");
+
+        //en
+        System.out.println(query.getLanguage());
+
+        //capital
+        System.out.println(query.getText());
+
+        //Searching [capital], using language: en
+        System.out.println(query);
+    }
+}
